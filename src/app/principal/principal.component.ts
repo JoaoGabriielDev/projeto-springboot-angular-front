2d10ae4c5be35e846f5a7669f821a7cbbc6dcf9b
JoaoGabriielDev <jgabriellima1067@gmail.com>
2024-03-11 12:00:57 -0300

    Listagem de clientes, ngOnInit and ngFor

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ClienteService } from '../servico/cliente.service';
+import { Cliente } from '../modelo/Cliente';
 
 @Component({
   selector: 'app-principal',
@@ -10,5 +12,20 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './principal.component.css'
 })
 export class PrincipalComponent {
+
+  btnCadastro:boolean = true;
+
+  clientes:Cliente[] = [];
+
+  constructor(private servico:ClienteService){}
+
+  selecionar():void{
+    this.servico.selecionar()
+    .subscribe(retorno => this.clientes = retorno);
+  }
+
+  ngOnInit(){
+    this.selecionar();
+  }
 
 }
